fad4b695adc2a035abe0a91c049dc42c5242024d
trippytresor <104313854+trippytresor@users.noreply.github.com>
2026-02-15 23:48:03 +0200

    Enable autoplay for Vimeo iframe in showreel app

--- components/mac/apps/showreel-app.tsx
+++ components/mac/apps/showreel-app.tsx
@@ -5,7 +5,7 @@ export function ShowreelApp() {
     <div className="flex h-full flex-col bg-black">
       <div className="relative flex-1">
         <iframe
-          src="https://player.vimeo.com/video/712930144?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          src="https://player.vimeo.com/video/712930144?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479?autoplay=1"
           className="absolute inset-0 h-full w-full"
           style={{ border: 0 }}
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
